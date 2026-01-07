efc4c9f770cbcd3ae11e3b8966a79c0648ade5d2
pessthx23-svg <pessthx23@gmail.com>
2026-01-07 19:51:33 +0700

    Add files via upload

---
+++ data/bundesliga.js
@@ -0,0 +1,61 @@
+const bundesliga = [
+  // Bayern Munich
+  { name:"Harry Kane", league:"Bundesliga", team:"Bayern Munich", position:"Forward", age:31, height:"188 cm", weight:"86 kg" },
+  { name:"Jamal Musiala", league:"Bundesliga", team:"Bayern Munich", position:"Attacking Midfielder", age:22, height:"184 cm", weight:"72 kg" },
+  { name:"Joshua Kimmich", league:"Bundesliga", team:"Bayern Munich", position:"Midfielder", age:30, height:"176 cm", weight:"75 kg" },
+  { name:"Leroy Sané", league:"Bundesliga", team:"Bayern Munich", position:"Winger", age:29, height:"183 cm", weight:"75 kg" },
+  { name:"Thomas Müller", league:"Bundesliga", team:"Bayern Munich", position:"Forward", age:35, height:"186 cm", weight:"75 kg" },
+  { name:"Manuel Neuer", league:"Bundesliga", team:"Bayern Munich", position:"Goalkeeper", age:38, height:"193 cm", weight:"92 kg" },
+  { name:"Alphonso Davies", league:"Bundesliga", team:"Bayern Munich", position:"Defender", age:24, height:"185 cm", weight:"77 kg" },
+
+  // Borussia Dortmund
+  { name:"Julian Brandt", league:"Bundesliga", team:"Borussia Dortmund", position:"Midfielder", age:29, height:"185 cm", weight:"83 kg" },
+  { name:"Marco Reus", league:"Bundesliga", team:"Borussia Dortmund", position:"Attacking Midfielder", age:35, height:"180 cm", weight:"71 kg" },
+  { name:"Karim Adeyemi", league:"Bundesliga", team:"Borussia Dortmund", position:"Forward", age:23, height:"180 cm", weight:"75 kg" },
+  { name:"Nico Schlotterbeck", league:"Bundesliga", team:"Borussia Dortmund", position:"Defender", age:25, height:"191 cm", weight:"85 kg" },
+  { name:"Mats Hummels", league:"Bundesliga", team:"Borussia Dortmund", position:"Defender", age:36, height:"191 cm", weight:"90 kg" },
+
+  // Bayer Leverkusen
+  { name:"Florian Wirtz", league:"Bundesliga", team:"Bayer Leverkusen", position:"Attacking Midfielder", age:21, height:"176 cm", weight:"70 kg" },
+  { name:"Victor Boniface", league:"Bundesliga", team:"Bayer Leverkusen", position:"Forward", age:24, height:"190 cm", weight:"88 kg" },
+  { name:"Granit Xhaka", league:"Bundesliga", team:"Bayer Leverkusen", position:"Midfielder", age:32, height:"185 cm", weight:"82 kg" },
+  { name:"Jeremie Frimpong", league:"Bundesliga", team:"Bayer Leverkusen", position:"Defender", age:24, height:"171 cm", weight:"63 kg" },
+
+  // RB Leipzig
+  { name:"Loïs Openda", league:"Bundesliga", team:"RB Leipzig", position:"Forward", age:25, height:"177 cm", weight:"75 kg" },
+  { name:"Xavi Simons", league:"Bundesliga", team:"RB Leipzig", position:"Attacking Midfielder", age:21, height:"168 cm", weight:"60 kg" },
+  { name:"Dani Olmo", league:"Bundesliga", team:"RB Leipzig", position:"Midfielder", age:26, height:"179 cm", weight:"72 kg" },
+  { name:"Willi Orbán", league:"Bundesliga", team:"RB Leipzig", position:"Defender", age:32, height:"186 cm", weight:"87 kg" },
+
+  // Eintracht Frankfurt
+  { name:"Omar Marmoush", league:"Bundesliga", team:"Eintracht Frankfurt", position:"Forward", age:26, height:"183 cm", weight:"77 kg" },
+  { name:"Mario Götze", league:"Bundesliga", team:"Eintracht Frankfurt", position:"Midfielder", age:32, height:"176 cm", weight:"72 kg" },
+
+  // Stuttgart
+  { name:"Serhou Guirassy", league:"Bundesliga", team:"VfB Stuttgart", position:"Forward", age:28, height:"187 cm", weight:"82 kg" },
+  { name:"Deniz Undav", league:"Bundesliga", team:"VfB Stuttgart", position:"Forward", age:28, height:"179 cm", weight:"79 kg" },
+
+  // Hoffenheim
+  { name:"Andrej Kramarić", league:"Bundesliga", team:"TSG Hoffenheim", position:"Forward", age:33, height:"177 cm", weight:"73 kg" },
+
+  // Wolfsburg
+  { name:"Jonas Wind", league:"Bundesliga", team:"VfL Wolfsburg", position:"Forward", age:26, height:"190 cm", weight:"88 kg" },
+
+  // Freiburg
+  { name:"Vincenzo Grifo", league:"Bundesliga", team:"SC Freiburg", position:"Winger", age:31, height:"180 cm", weight:"75 kg" },
+
+  // Mainz
+  { name:"Jonathan Burkardt", league:"Bundesliga", team:"Mainz 05", position:"Forward", age:24, height:"181 cm", weight:"78 kg" },
+
+  // Union Berlin
+  { name:"Kevin Behrens", league:"Bundesliga", team:"Union Berlin", position:"Forward", age:34, height:"185 cm", weight:"83 kg" },
+
+  // Werder Bremen
+  { name:"Niclas Füllkrug", league:"Bundesliga", team:"Werder Bremen", position:"Forward", age:31, height:"189 cm", weight:"85 kg" },
+
+  // Borussia Mönchengladbach
+  { name:"Alassane Pléa", league:"Bundesliga", team:"Borussia Mönchengladbach", position:"Forward", age:32, height:"181 cm", weight:"75 kg" },
+
+  // Augsburg
+  { name:"Ermedin Demirović", league:"Bundesliga", team:"Augsburg", position:"Forward", age:27, height:"185 cm", weight:"84 kg" }
+];
