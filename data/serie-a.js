e4f0db01e34b54194d6afbcee9e6c1bb5f23bf8c
pessthx23-svg <pessthx23@gmail.com>
2026-01-07 21:01:50 +0700

    Update serie-a.js

--- data/serie-a.js
+++ data/serie-a.js
@@ -1,56 +1,54 @@
-window.serieA = [
-  // Inter Milan
-  { name:"Lautaro Martínez", league:"Serie A", team:"Inter Milan", position:"Forward", age:27, height:"174 cm", weight:"72 kg" },
-  { name:"Marcus Thuram", league:"Serie A", team:"Inter Milan", position:"Forward", age:27, height:"192 cm", weight:"90 kg" },
-  { name:"Nicolò Barella", league:"Serie A", team:"Inter Milan", position:"Midfielder", age:28, height:"172 cm", weight:"68 kg" },
-  { name:"Hakan Çalhanoğlu", league:"Serie A", team:"Inter Milan", position:"Midfielder", age:31, height:"178 cm", weight:"76 kg" },
-  { name:"Alessandro Bastoni", league:"Serie A", team:"Inter Milan", position:"Defender", age:25, height:"190 cm", weight:"75 kg" },
+window.serieAPlayers = [
+  { name: "Lautaro Martínez", team: "Inter", position: "Forward", age: 27, league: "Serie A" },
+  { name: "Marcus Thuram", team: "Inter", position: "Forward", age: 27, league: "Serie A" },
+  { name: "Hakan Çalhanoğlu", team: "Inter", position: "Midfielder", age: 31, league: "Serie A" },
+  { name: "Nicolò Barella", team: "Inter", position: "Midfielder", age: 27, league: "Serie A" },
+  { name: "Alessandro Bastoni", team: "Inter", position: "Defender", age: 25, league: "Serie A" },
 
-  // AC Milan
-  { name:"Rafael Leão", league:"Serie A", team:"AC Milan", position:"Winger", age:25, height:"188 cm", weight:"81 kg" },
-  { name:"Christian Pulisic", league:"Serie A", team:"AC Milan", position:"Winger", age:26, height:"177 cm", weight:"69 kg" },
-  { name:"Theo Hernández", league:"Serie A", team:"AC Milan", position:"Defender", age:27, height:"184 cm", weight:"81 kg" },
-  { name:"Mike Maignan", league:"Serie A", team:"AC Milan", position:"Goalkeeper", age:29, height:"191 cm", weight:"89 kg" },
+  { name: "Rafael Leão", team: "AC Milan", position: "Forward", age: 25, league: "Serie A" },
+  { name: "Olivier Giroud", team: "AC Milan", position: "Forward", age: 38, league: "Serie A" },
+  { name: "Theo Hernández", team: "AC Milan", position: "Defender", age: 27, league: "Serie A" },
+  { name: "Mike Maignan", team: "AC Milan", position: "Goalkeeper", age: 29, league: "Serie A" },
 
-  // Juventus
-  { name:"Dušan Vlahović", league:"Serie A", team:"Juventus", position:"Forward", age:25, height:"190 cm", weight:"85 kg" },
-  { name:"Federico Chiesa", league:"Serie A", team:"Juventus", position:"Winger", age:27, height:"175 cm", weight:"70 kg" },
-  { name:"Adrien Rabiot", league:"Serie A", team:"Juventus", position:"Midfielder", age:29, height:"191 cm", weight:"80 kg" },
-  { name:"Manuel Locatelli", league:"Serie A", team:"Juventus", position:"Midfielder", age:27, height:"185 cm", weight:"75 kg" },
+  { name: "Dušan Vlahović", team: "Juventus", position: "Forward", age: 25, league: "Serie A" },
+  { name: "Federico Chiesa", team: "Juventus", position: "Forward", age: 27, league: "Serie A" },
+  { name: "Adrien Rabiot", team: "Juventus", position: "Midfielder", age: 29, league: "Serie A" },
+  { name: "Manuel Locatelli", team: "Juventus", position: "Midfielder", age: 27, league: "Serie A" },
 
-  // Napoli
-  { name:"Victor Osimhen", league:"Serie A", team:"Napoli", position:"Forward", age:26, height:"186 cm", weight:"78 kg" },
-  { name:"Khvicha Kvaratskhelia", league:"Serie A", team:"Napoli", position:"Winger", age:24, height:"183 cm", weight:"70 kg" },
-  { name:"Stanislav Lobotka", league:"Serie A", team:"Napoli", position:"Midfielder", age:30, height:"170 cm", weight:"60 kg" },
+  { name: "Paulo Dybala", team: "AS Roma", position: "Forward", age: 31, league: "Serie A" },
+  { name: "Romelu Lukaku", team: "AS Roma", position: "Forward", age: 31, league: "Serie A" },
+  { name: "Lorenzo Pellegrini", team: "AS Roma", position: "Midfielder", age: 28, league: "Serie A" },
 
-  // Roma
-  { name:"Paulo Dybala", league:"Serie A", team:"AS Roma", position:"Forward", age:31, height:"177 cm", weight:"75 kg" },
-  { name:"Romelu Lukaku", league:"Serie A", team:"AS Roma", position:"Forward", age:31, height:"191 cm", weight:"94 kg" },
-  { name:"Lorenzo Pellegrini", league:"Serie A", team:"AS Roma", position:"Midfielder", age:28, height:"186 cm", weight:"77 kg" },
+  { name: "Victor Osimhen", team: "Napoli", position: "Forward", age: 26, league: "Serie A" },
+  { name: "Khvicha Kvaratskhelia", team: "Napoli", position: "Forward", age: 23, league: "Serie A" },
+  { name: "Stanislav Lobotka", team: "Napoli", position: "Midfielder", age: 30, league: "Serie A" },
 
-  // Lazio
-  { name:"Ciro Immobile", league:"Serie A", team:"Lazio", position:"Forward", age:35, height:"185 cm", weight:"80 kg" },
-  { name:"Sergej Milinković-Savić", league:"Serie A", team:"Lazio", position:"Midfielder", age:29, height:"192 cm", weight:"85 kg" },
+  { name: "Gianluigi Donnarumma", team: "PSG", position: "Goalkeeper", age: 26, league: "Serie A" }, // อดีต Serie A icon
+  { name: "Teun Koopmeiners", team: "Atalanta", position: "Midfielder", age: 26, league: "Serie A" },
+  { name: "Ademola Lookman", team: "Atalanta", position: "Forward", age: 27, league: "Serie A" },
 
-  // Atalanta
-  { name:"Ademola Lookman", league:"Serie A", team:"Atalanta", position:"Forward", age:27, height:"174 cm", weight:"70 kg" },
-  { name:"Teun Koopmeiners", league:"Serie A", team:"Atalanta", position:"Midfielder", age:27, height:"184 cm", weight:"78 kg" },
+  { name: "Ciro Immobile", team: "Lazio", position: "Forward", age: 35, league: "Serie A" },
+  { name: "Luis Alberto", team: "Lazio", position: "Midfielder", age: 32, league: "Serie A" },
 
-  // Fiorentina
-  { name:"Nicolás González", league:"Serie A", team:"Fiorentina", position:"Winger", age:26, height:"180 cm", weight:"75 kg" },
+  { name: "Joshua Zirkzee", team: "Bologna", position: "Forward", age: 23, league: "Serie A" },
+  { name: "Riccardo Orsolini", team: "Bologna", position: "Forward", age: 28, league: "Serie A" },
 
-  // Bologna
-  { name:"Riccardo Orsolini", league:"Serie A", team:"Bologna", position:"Winger", age:28, height:"183 cm", weight:"72 kg" },
+  { name: "Domenico Berardi", team: "Sassuolo", position: "Forward", age: 30, league: "Serie A" },
+  { name: "Andrea Pinamonti", team: "Sassuolo", position: "Forward", age: 26, league: "Serie A" },
 
-  // Torino
-  { name:"Duván Zapata", league:"Serie A", team:"Torino", position:"Forward", age:33, height:"186 cm", weight:"88 kg" },
+  { name: "Albert Guðmundsson", team: "Genoa", position: "Forward", age: 27, league: "Serie A" },
+  { name: "Mateo Retegui", team: "Genoa", position: "Forward", age: 25, league: "Serie A" },
 
-  // Monza
-  { name:"Andrea Colpani", league:"Serie A", team:"Monza", position:"Midfielder", age:25, height:"184 cm", weight:"74 kg" },
+  { name: "Antonio Candreva", team: "Salernitana", position: "Midfielder", age: 38, league: "Serie A" },
+  { name: "Boulaye Dia", team: "Salernitana", position: "Forward", age: 28, league: "Serie A" },
 
-  // Udinese
-  { name:"Beto", league:"Serie A", team:"Udinese", position:"Forward", age:27, height:"194 cm", weight:"88 kg" },
+  { name: "Roberto Pereyra", team: "Udinese", position: "Midfielder", age: 34, league: "Serie A" },
+  { name: "Lazar Samardžić", team: "Udinese", position: "Midfielder", age: 23, league: "Serie A" },
 
-  // Sassuolo
-  { name:"Domenico Berardi", league:"Serie A", team:"Sassuolo", position:"Winger", age:30, height:"183 cm", weight:"72 kg" }
+  { name: "Nikola Milenković", team: "Fiorentina", position: "Defender", age: 27, league: "Serie A" },
+  { name: "Giacomo Bonaventura", team: "Fiorentina", position: "Midfielder", age: 35, league: "Serie A" },
+
+  { name: "Tammy Abraham", team: "AS Roma", position: "Forward", age: 27, league: "Serie A" },
+  { name: "Stephan El Shaarawy", team: "AS Roma", position: "Forward", age: 32, league: "Serie A" }
 ];
+
