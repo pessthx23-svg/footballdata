24d094d99b4f669659d11b81fd086a4af9897d22
pessthx23-svg <pessthx23@gmail.com>
2026-01-07 20:31:36 +0700

    Update premier-league.js

--- data/premier-league.js
+++ data/premier-league.js
@@ -1,20 +1,55 @@
 window.premierLeaguePlayers = [
-  {
-    name: "Erling Haaland",
-    team: "Manchester City",
-    position: "Forward",
-    age: 24
-  },
-  {
-    name: "Kevin De Bruyne",
-    team: "Manchester City",
-    position: "Midfielder",
-    age: 33
-  },
-  {
-    name: "Mohamed Salah",
-    team: "Liverpool",
-    position: "Forward",
-    age: 32
-  }
+  // Manchester City
+  { name: "Erling Haaland", team: "Manchester City", position: "Forward", age: 24, league: "Premier League" },
+  { name: "Kevin De Bruyne", team: "Manchester City", position: "Midfielder", age: 33, league: "Premier League" },
+  { name: "Phil Foden", team: "Manchester City", position: "Midfielder", age: 24, league: "Premier League" },
+  { name: "Bernardo Silva", team: "Manchester City", position: "Midfielder", age: 30, league: "Premier League" },
+  { name: "Rodri", team: "Manchester City", position: "Midfielder", age: 28, league: "Premier League" },
+  { name: "Ruben Dias", team: "Manchester City", position: "Defender", age: 27, league: "Premier League" },
+  { name: "Ederson", team: "Manchester City", position: "Goalkeeper", age: 31, league: "Premier League" },
+
+  // Arsenal
+  { name: "Bukayo Saka", team: "Arsenal", position: "Winger", age: 23, league: "Premier League" },
+  { name: "Martin Ødegaard", team: "Arsenal", position: "Midfielder", age: 26, league: "Premier League" },
+  { name: "Declan Rice", team: "Arsenal", position: "Midfielder", age: 26, league: "Premier League" },
+  { name: "Gabriel Jesus", team: "Arsenal", position: "Forward", age: 27, league: "Premier League" },
+  { name: "William Saliba", team: "Arsenal", position: "Defender", age: 23, league: "Premier League" },
+  { name: "Kai Havertz", team: "Arsenal", position: "Forward", age: 25, league: "Premier League" },
+
+  // Liverpool
+  { name: "Mohamed Salah", team: "Liverpool", position: "Forward", age: 32, league: "Premier League" },
+  { name: "Virgil van Dijk", team: "Liverpool", position: "Defender", age: 33, league: "Premier League" },
+  { name: "Trent Alexander-Arnold", team: "Liverpool", position: "Defender", age: 26, league: "Premier League" },
+  { name: "Alisson Becker", team: "Liverpool", position: "Goalkeeper", age: 32, league: "Premier League" },
+  { name: "Dominik Szoboszlai", team: "Liverpool", position: "Midfielder", age: 24, league: "Premier League" },
+  { name: "Luis Díaz", team: "Liverpool", position: "Winger", age: 28, league: "Premier League" },
+
+  // Manchester United
+  { name: "Bruno Fernandes", team: "Manchester United", position: "Midfielder", age: 30, league: "Premier League" },
+  { name: "Marcus Rashford", team: "Manchester United", position: "Forward", age: 27, league: "Premier League" },
+  { name: "Casemiro", team: "Manchester United", position: "Midfielder", age: 32, league: "Premier League" },
+  { name: "Lisandro Martínez", team: "Manchester United", position: "Defender", age: 26, league: "Premier League" },
+  { name: "Rasmus Højlund", team: "Manchester United", position: "Forward", age: 21, league: "Premier League" },
+
+  // Chelsea
+  { name: "Cole Palmer", team: "Chelsea", position: "Midfielder", age: 22, league: "Premier League" },
+  { name: "Enzo Fernández", team: "Chelsea", position: "Midfielder", age: 24, league: "Premier League" },
+  { name: "Reece James", team: "Chelsea", position: "Defender", age: 25, league: "Premier League" },
+  { name: "Nicolas Jackson", team: "Chelsea", position: "Forward", age: 23, league: "Premier League" },
+  { name: "Thiago Silva", team: "Chelsea", position: "Defender", age: 40, league: "Premier League" },
+
+  // Tottenham
+  { name: "Son Heung-min", team: "Tottenham", position: "Forward", age: 32, league: "Premier League" },
+  { name: "James Maddison", team: "Tottenham", position: "Midfielder", age: 28, league: "Premier League" },
+  { name: "Cristian Romero", team: "Tottenham", position: "Defender", age: 26, league: "Premier League" },
+  { name: "Micky van de Ven", team: "Tottenham", position: "Defender", age: 23, league: "Premier League" },
+
+  // Newcastle
+  { name: "Alexander Isak", team: "Newcastle United", position: "Forward", age: 25, league: "Premier League" },
+  { name: "Bruno Guimarães", team: "Newcastle United", position: "Midfielder", age: 27, league: "Premier League" },
+  { name: "Kieran Trippier", team: "Newcastle United", position: "Defender", age: 34, league: "Premier League" },
+
+  // Aston Villa
+  { name: "Ollie Watkins", team: "Aston Villa", position: "Forward", age: 29, league: "Premier League" },
+  { name: "Douglas Luiz", team: "Aston Villa", position: "Midfielder", age: 26, league: "Premier League" }
 ];
