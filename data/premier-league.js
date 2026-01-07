23f3fee339d08f9ed62416e6cccbaeaed12a6c8f
pessthx23-svg <pessthx23@gmail.com>
2026-01-07 19:34:27 +0700

    update premier league players

--- data/premier-league.js
+++ data/premier-league.js
@@ -1,1 +1,15 @@
+window.premierLeaguePlayers = [
+  {
+    name: "Erling Haaland",
+    team: "Manchester City",
+    position: "Forward",
+    age: 24
+  },
+  {
+    name: "Kevin De Bruyne",
+    team: "Manchester City",
+    position: "Midfielder",
+    age: 33
+  }
+];
 
