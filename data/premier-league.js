8df56e7373b06a73e7c6491ef2a1ae1311b11c2b
pessthx23-svg <pessthx23@gmail.com>
2026-01-07 20:21:47 +0700

    Update premier-league.js

--- data/premier-league.js
+++ data/premier-league.js
@@ -1,20 +1,7 @@
 window.premierLeaguePlayers = [
-  {
-    name: "Erling Haaland",
-    team: "Manchester City",
-    position: "Forward",
-    age: 24
-  },
-  {
-    name: "Kevin De Bruyne",
-    team: "Manchester City",
-    position: "Midfielder",
-    age: 33
-  },
-  {
-    name: "Mohamed Salah",
-    team: "Liverpool",
-    position: "Forward",
-    age: 32
-  }
+  { name: "Erling Haaland", team: "Manchester City", position: "Forward", age: 24, league: "Premier League" },
+  { name: "Kevin De Bruyne", team: "Manchester City", position: "Midfielder", age: 33, league: "Premier League" },
+  { name: "Mohamed Salah", team: "Liverpool", position: "Forward", age: 32, league: "Premier League" },
+  { name: "Bukayo Saka", team: "Arsenal", position: "Winger", age: 23, league: "Premier League" }
+  // 👉 ใส่ต่อให้ครบ 40 คน
 ];
